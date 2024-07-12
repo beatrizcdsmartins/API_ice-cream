7cfff8a10fa2e2fbe5db239d31b5e32d0646c69d
beatrizcdsmartins <beatrizcdsmartins@gmail.com>
2024-07-12 15:58:42 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -4,15 +4,15 @@ import './database'
 import { resolve } from 'path'
 import cors from 'cors'
 
-// const corsOptions = {
-//   origin: 'https://ice-cream-interface.vercel.app/',
-//   credentials: true,
-// }
+const corsOptions = {
+  origin: 'https://ice-cream-interface.vercel.app/',
+  credentials: true,
+}
 
 class App {
   constructor() {
     this.app = express()
-    this.app.use(cors())
+    this.app.use(cors(corsOptions))
     this.middlewares()
     this.routes()
   }
